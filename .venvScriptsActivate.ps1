cdabcd094788bd4f98b87865dfbd29d52226f3ef
Anant2006-lang <anantbedi30@gmail.com>
2026-04-03 22:49:30 +0530

    new setup added

---
+++ .venvScriptsActivate.ps1
@@ -0,0 +1,3 @@
+      PID    PPID    PGID     WINPID   TTY         UID    STIME COMMAND
+      930     685     930     111420  cons0     197609 22:38:01 /usr/bin/PS
+      685       1     685      70268  cons0     197609 22:30:55 /usr/bin/bash
